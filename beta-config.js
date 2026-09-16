06b1371fb9a8a2480ca6ac1cd819c4a06e73182d
RebatifyApp <support.rebatifyapp@gmail.com>
2026-09-15 21:37:15 -0700

    Add files via upload

--- beta-config.js
+++ beta-config.js
@@ -1,3 +1,3 @@
 /* Rebatify Beta signup endpoint.
    After deploying the included Google Apps Script backend, paste the Web App URL below. */
-window.REBATIFY_BETA_ENDPOINT = "";
+window.REBATIFY_BETA_ENDPOINT = "https://script.google.com/macros/s/AKfycbyvBiwHD86uJ9dzEuT0ON-Q9xLf41ngyYSmdcGbjc3gFECDLaf9SS6zGgy3VHogYKrhkA/exec";
